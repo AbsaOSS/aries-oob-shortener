31cd563b3bbb6662603632517393bcb9547fe4df
Miroslav Kovar <miroslav.kovar@absa.africa>
2022-08-02 14:13:12 +0200

    Publish docker image as part of the workflow
    Signed-off-by: Miroslav Kovar <miroslav.kovar@absa.africa>

--- ci/build.test.sh
+++ ci/build.test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -ex
 
+DOCKER_IMG_NAME=${1:-"dlt-shortener-test"}
+VERSION=${2:-"0.0.1"}
+
 SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-BASE_IMAGE="dlt-shortener-base:0.0.1"
+BASE_IMAGE="dlt-shortener-base:$VERSION"
+FULL_DOCKER_IMG_NAME="$DOCKER_IMG_NAME:$VERSION"
 
 docker build -f $SCRIPT_DIR/Dockerfile.base -t $BASE_IMAGE $SCRIPT_DIR/..
-docker build -f $SCRIPT_DIR/Dockerfile.test -t dlt-shortener-test:0.0.1 --build-arg "BASE_IMAGE=$BASE_IMAGE" $SCRIPT_DIR/..
+docker build -f $SCRIPT_DIR/Dockerfile.test -t $FULL_DOCKER_IMG_NAME --build-arg "BASE_IMAGE=$BASE_IMAGE" $SCRIPT_DIR/..
